daf5f387fa6a1edc6055948b6bfdf1af5109ff40
Dj-gamer007 <135929745+Dj-gamer007@users.noreply.github.com>
2024-02-20 21:17:07 +0530

    Update quiz game.py

--- quiz game.py
+++ quiz game.py
@@ -2,36 +2,44 @@ print("Welcome to my computer quiz!")
 
 playing=input("do you want to play?")
 
-if playing != yes:
+if playing.lower() != yes:
   quit()
 print("Okay! let's play:)")
+score = 0
 
-answer=input("""What is the highest individual score ever recorded in Test cricke?""")
-if answer == "400":
+answer=input("""What is the highest individual score ever recorded in Test cricket?""")
+if answer.lower() == "400":
   print("Correct!")
+  score += 1
 else:
   print("Incorrect!")
 
 answer=input("""In cricket, what is the term used to describe when a bowler dismisses a batsman with three consecutive deliveries?""")
-if answer == "hat-trick":
+if answer.lower() == "hat-trick":
   print("Correct!")
+  score += 1
 else:
   print("Incorrect!")
 
 answer=input("""Which country won the ICC Cricket World Cup in 2019?""")
-if answer == "England":
+if answer.lower() == "England":
   print("Correct!")
+  score += 1
 else:
   print("Incorrect!")
 
 answer=input("""Who holds the record for the fastest century in One Day International (ODI) cricket?""")
-if answer == "AB de Villiers":
+if answer.lower() == "AB de Villiers":
   print("Correct!")
+  score += 1
 else:
   print("Incorrect!")
 
 answer=input("""What does LBW stand for in cricket?""")
-if answer == "Leg Before Wicket(LBW)":
+if answer.lower() == "Leg Before Wicket(LBW)":
   print("Correct!")
+  score += 1
 else:
   print("Incorrect!")
+print("You got"+ str(score) + "questions correct!")
+print("You got"+ str((score/4)*100) + "%.")
